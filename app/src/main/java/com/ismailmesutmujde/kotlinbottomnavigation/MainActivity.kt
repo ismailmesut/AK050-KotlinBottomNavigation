c350973de8a6a28816905669eb602f41cab070d9
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-20 14:44:35 +0300

    updated: coding process

--- app/src/main/java/com/ismailmesutmujde/kotlinbottomnavigation/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinbottomnavigation/MainActivity.kt
@@ -5,11 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.ismailmesutmujde.kotlinbottomnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(bindingMain.bottomNav, navHostFragment.navController)
     }
 }
